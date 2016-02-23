6a896b98be57705339648ee5eb7c230b1e33df1d
Thibault <thibaultlg@gmail.com>
2016-02-23 22:14:01 +0100

    Test

--- CESB/CESB/ConnexionDataSource.cs
+++ CESB/CESB/ConnexionDataSource.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace CESB
 {
     public static class ConnexionDataSource
     {
+        private SqlConnection connection;
+
+        public bool SeConnecter()
+        {
+            this.connection = new SqlConnection();
+        }
+
     }
 }
